f2852ee20a8923262c3c199bc71b92930059f1a1
artlosk <artlosk@gmail.com>
2022-03-16 19:43:56 +0600

    first commit

---
+++ phpmyadmin/config.user.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: krok
+ * Date: 14.01.17
+ * Time: 21:18
+ */
+
+$cfg['VersionCheck'] = false;
+$cfg['DefaultCharset'] = 'utf8';
+$cfg['DefaultConnectionCollation'] = 'utf8_unicode_ci';
